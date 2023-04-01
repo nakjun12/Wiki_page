f3f498add10543d225b753f70636ecf686e336ed
nakjun <wns450@gmail.com>
2023-04-02 02:06:00 +0900

    정규 표현식 활용하여 찾기

--- Wiki_Page/src/pages/Wiki.jsx
+++ Wiki_Page/src/pages/Wiki.jsx
@@ -1,49 +1,19 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams, Link } from "react-router-dom";
 import WikiList from "../components/WikiList";
 import ButtonComponent from "../components/ButtonComponent";
 import { useNavigate } from "react-router";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
-const dbData = [
-  {
-    title: "감자",
-    content: "<div>ㅇㅇㅇ <a href=`/wiki'>감자</a></div>",
-    id: 1,
-  },
-  {
-    title: "감자김치",
-    content: "<p>ㅇㅇㅇ</p>",
-    id: 2,
-  },
-  {
-    title: "감자김치31111",
-    content: "<p>ㅇㅇㅇ</p>",
-    id: 3,
-  },
-  {
-    title: "감자김치4111111111111",
-    content: "<p>ㅇㅇㅇ</p>",
-    id: 4,
-  },
-  {
-    title: "감자김치511111111111111",
-    content: "<p>ㅇㅇㅇ</p>",
-    id: 5,
-  },
-  {
-    title: "감자김치6",
-    content: "<p>ㅇㅇㅇ</p>",
-    id: 6,
-  },
-]; //위키 필터 돌려서 dbdata처럼 돌리자
+//위키 필터 돌려서 dbdata처럼 돌리자
 const Wiki = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [wikiList, setwikiList] = useState([]);
+  const [[first, second, third], setComent] = useState(["", "", ""]);
   const location = useLocation();
   const navigate = useNavigate();
   const { id } = useParams();
-  console.log(id);
+
   useEffect(() => {
     if (location.state) {
       setwikiList(location.state.wikiList);
@@ -60,7 +30,6 @@ const Wiki = () => {
           });
           setTitle(idData[0].title);
           setContent(idData[0].content);
-          console.log(idData);
         });
     }
   }, [location.state, id]);
@@ -69,6 +38,60 @@ const Wiki = () => {
     navigate("/editor", { state: { data: { title, content, id } } });
   };
 
+  const includedTitles = wikiList
+    .filter((el) => content.includes(el.title))
+    .sort((a, b) => b.title.length - a.title.length);
+
+  const fixResult = () => {
+    const sortedTitles = includedTitles.sort(
+      (a, b) => b.title.length - a.title.length
+    );
+
+    let newContent = content;
+    let lastIndex = 0;
+
+    sortedTitles.forEach((titleObj) => {
+      const { id, title } = titleObj;
+      const regex = new RegExp(`${title}`, "g");
+      let match;
+
+      while ((match = regex.exec(newContent)) !== null) {
+        console.log("하이");
+        if (match.index < lastIndex) {
+          continue; // 이미 링크로 변경된 부분은 건너뛰기
+        }
+
+        const link = `<a href="http://localhost:3000/wiki/${id}">${title}</a>`;
+        newContent =
+          newContent.slice(0, match.index) +
+          link +
+          newContent.slice(match.index + title.length);
+        console.log(newContent);
+        lastIndex = match.index + link.length;
+      }
+    });
+
+    console.log(newContent);
+  };
+  // "<a href=\"http://localhost:3000/wiki/3\">키아</a>"
+  fixResult();
+  const result = '<a href="http://localhost:3000/wiki/3">키아</a>';
+  const newComent = (
+    <p>
+      감자 싫어요 <Link to=" / ">Home</Link>김치최고
+    </p>
+  );
+
+  //플랜 1
+  // 검증뒤에 성공하면
+  // 타이틀과 뉴 코멘트 백업
+  // 기존 코멘트로 검증한다.
+
+  //1번 예시 이세돌은 멋있따.
+
+  // 전체 검증하는 법
+  // 만약에 검증되면 replace해줌
+  //같은 것을 만나면 링크로 바꿔주고
   return (
     <>
       <section className="text-gray-600 body-font">
@@ -78,11 +101,16 @@ const Wiki = () => {
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 bg-white text-gray-900 border-2 pb-3">
               {title}
             </h1>
-            <div className="leading-relaxed text-base bg-white">{content}</div>
+            <div
+              dangerouslySetInnerHTML={{ __html: result }}
+              // 위험하지 않은 콘텐츠가 제공되면서도 마크업을 동적으로 생성하기 위한 방법입니다.
+              // 하지만 위험한 콘텐츠는 삽입하지 않도록 주의해야 합니다.
+            />
+            {content}
           </div>
         </div>
       </section>
-      <WikiList wikiList={dbData} size="py-2" />
+      <WikiList wikiList={includedTitles} size="py-2" />
     </>
   );
 };
